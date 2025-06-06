8923992f96729f7695550ec0d8f28cb2f3102780
spotfreeman <r.oyarzun83@gmail.com>
2025-06-05 22:17:25 -0400

    Implementando Modulo Proyectos

--- src/models/proyectos.model.js
+++ src/models/proyectos.model.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
+
 const proyectoSchema = new mongoose.Schema({
     nombre: { type: String, required: true },
     codigo: { type: String, required: true },
     descripcion: { type: String, required: true },
     estado: { type: String, required: true },
+    pagos: [{
+        fecha_hora: { type: Date, required: true },
+        monto: { type: Number, required: true },
+        descripcion: { type: String, required: true }
+    }],
+    avance: [{
+        fecha_hora: { type: Date, required: true },
+        porcentaje: { type: Number, required: true },
+        descripcion: { type: String, required: true }
+    }],
+
+
 
 }, { versionKey: false });
+
 module.exports = mongoose.model('proyecto', proyectoSchema, 'proyectos'); // 'proyectos' is the collection name
