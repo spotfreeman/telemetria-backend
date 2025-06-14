c7694943e3664e365ab87cfcb29dbaece732cb96
spotfreeman <r.oyarzun83@gmail.com>
2025-06-14 16:10:15 -0400

    Implementando Unity

--- src/models/unity.model.js
+++ src/models/unity.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const unitySchema = new mongoose.Schema({
-    user: { type: String, required: true },
+    username: { type: String, required: true },
     password: { type: String, required: true },
     level: { type: Number },
     coin: { type: Number }
