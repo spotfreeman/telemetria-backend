1f462fcafb6d41c1dde36c94dec7fd92023522d4
spotfreeman <r.oyarzun83@gmail.com>
2025-06-15 00:00:50 -0400

    Test Unity

--- src/models/unity.model.js
+++ src/models/unity.model.js
@@ -4,6 +4,6 @@ const unitySchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     level: { type: Number },
-    coin: { type: Number }
+    coins: { type: Number }
 }, { versionKey: false });
 module.exports = mongoose.model('Unity', unitySchema, 'unitys');
