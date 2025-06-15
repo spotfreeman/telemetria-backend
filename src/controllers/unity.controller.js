84fbdf8828ae24e945f8a3164edf925886276817
spotfreeman <r.oyarzun83@gmail.com>
2025-06-14 20:18:51 -0400

    Test Unity

--- src/controllers/unity.controller.js
+++ src/controllers/unity.controller.js
@@ -15,8 +15,17 @@ exports.post = async (req, res) => {
             return res.status(400).json({ error: 'Contraseña incorrecta' });
         }
 
+        // Si la contraseña es correcta, devolver los datos del usuario
+        return res.json({
+            username: user.username,
+            level: user.level,
+            coins: user.coins,
+        });
+
         // Login exitoso
-        return res.json({ message: 'Login exitoso', username: user.username });
+        // return res.json({ message: 'Login exitoso', username: user.username });
+
+
     } catch (err) {
         console.error('Error en login Unity:', err);
         res.status(500).json({ error: 'Error al crear la unidad' });
