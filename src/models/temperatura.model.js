393dc89ff4a92578cce6bdcfa57a57cba132db7d
spotfreeman <r.oyarzun83@gmail.com>
2025-06-21 13:30:31 -0400

    Actualizando temperatura.model

--- src/models/temperatura.model.js
+++ src/models/temperatura.model.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const temperaturaSchema = new mongoose.Schema({
     fecha_hora: { type: Date, required: true },
-    temperatura: { type: Number, required: true }
+    temperatura: { type: Number, required: true },
+    almacenamiento: { type: Number },
 }, { versionKey: false });
 
 module.exports = mongoose.model('temperatura', temperaturaSchema, 'temperaturas');
