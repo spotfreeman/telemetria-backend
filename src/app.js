903013cb68be15aad0899d890802455e2fac83d7
spotfreeman <r.oyarzun83@gmail.com>
2025-07-12 00:07:37 -0400

    ESP32

--- src/app.js
+++ src/app.js
@@ -11,6 +11,9 @@ const archivosRoutes = require('./routes/archivos.routes');
 const vacacionesRoutes = require('./routes/vacaciones.routes');
 const serveripRoutes = require('./routes/serverip.routes');
 
+// Importar las Rutas de ESP32
+const esp32Routes = require('./esp32/esp32.routes');
+
 // Test Unity MongoDB
 const unityRoutes = require('./routes/unity.routes');
 
@@ -39,6 +42,7 @@ app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/archivos', archivosRoutes);
 app.use('/api/vacaciones', vacacionesRoutes)
 app.use('/api/serverip', serveripRoutes);
+app.use('/api/esp32', esp32Routes);
 
 // Test Unity MongoDB
 app.use('/api/unity', unityRoutes);
--- src/esp32/esp32.controller.js
+++ src/esp32/esp32.controller.js
@@ -1,1 +1,36 @@
-const esp32
+const Esp32 = require('../models/esp32.model.js');
+
+exports.getAll = async (req, res) => {
+    try {
+        const allEsp32 = await Esp32.find();
+        res.json(allEsp32);
+    } catch (err) {
+        console.error('Error al obtener dispositivos ESP32:', err);
+        res.status(500).json({ error: 'Error al obtener dispositivos ESP32' });
+    }
+}
+
+exports.create = async (req, res) => {
+    try {
+        const newEsp32 = new Esp32(req.body);
+        await newEsp32.save();
+        res.status(201).json(newEsp32);
+    } catch (err) {
+        console.error('Error al crear el dispositivo ESP32:', err);
+        res.status(500).json({ error: 'Error al crear el dispositivo ESP32' });
+    }
+}
+
+exports.delete = async (req, res) => {
+    try {
+        const deletedEsp32 = await Esp32.findByIdAndDelete(req.params.id);
+        if (!deletedEsp32) {
+            return res.status(404).json({ error: 'Dispositivo ESP32 no encontrado' });
+        }
+        res.json({ message: 'Dispositivo ESP32 eliminado' });
+    } catch (err) {
+        console.error('Error al eliminar el dispositivo ESP32:', err);
+        res.status(500).json({ error: 'Error al eliminar el dispositivo ESP32' });
+    }
+}
+
