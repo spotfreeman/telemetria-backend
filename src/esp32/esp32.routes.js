fae107bd053c566cbd02dd626b6452abc41d0a2f
spotfreeman <r.oyarzun83@gmail.com>
2025-07-12 00:11:05 -0400

    ESP32

--- src/esp32/esp32.routes.js
+++ src/esp32/esp32.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const esp32Controller = require('./esp32.controller');
+const auth = require('../middleware/auth');
 
 router.get('/', esp32Controller.getAll);
-router.put('/:id', esp32Controller.update);
+router.get('/:id', esp32Controller.getById);
+router.post('/', esp32Controller.create);
+router.delete('/:id', esp32Controller.delete);
 
 module.exports = router;
