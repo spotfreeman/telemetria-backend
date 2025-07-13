3001f941a95d2ca405f2b9c4de535141e7e1db42
spotfreeman <r.oyarzun83@gmail.com>
2025-07-13 13:28:23 -0400

    ESP32 Cambiando API

--- src/esp32/esp32.routes.js
+++ src/esp32/esp32.routes.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const esp32Controller = require('./esp32.controller');
 const auth = require('../middleware/auth');
 
-router.use(auth); // Middleware de autenticación para todas las rutas
-
 router.get('/', esp32Controller.getAll);
 router.post('/', esp32Controller.create);
 //router.post('/', esp32Controller.post); // Ruta para recibir datos del ESP32
