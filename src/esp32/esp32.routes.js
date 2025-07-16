b263bcd2a380cdb140d0f3f4b3d29c1f221dc0a3
spotfreeman <r.oyarzun83@gmail.com>
2025-07-15 23:38:44 -0400

    Reparando ESP32 model controller

--- src/esp32/esp32.routes.js
+++ src/esp32/esp32.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const esp32Controller = require('./esp32.controller');
 
 router.get('/', esp32Controller.getAll);
-router.post('/', esp32Controller.create);
+router.post('/', esp32Controller.post);
 
 module.exports = router;
