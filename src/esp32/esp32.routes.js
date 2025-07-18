eb55d600200fbeeb6c137e31df52d88ad835c2b8
spotfreeman <r.oyarzun83@gmail.com>
2025-07-17 23:31:35 -0400

    Error ESP32

--- src/esp32/esp32.routes.js
+++ src/esp32/esp32.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const esp32Controller = require('./esp32.controller');
+const auth = require('../middleware/auth');
+
+router.use(auth); // Middleware de autenticación para todas las rutas
 
 router.get('/', esp32Controller.get);
 router.get('/:deviceId', esp32Controller.getAll);
