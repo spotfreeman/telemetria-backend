6a5d37284103458ed846e6ee3c83c2f85a1b6a6d
spotfreeman <r.oyarzun83@gmail.com>
2025-07-17 23:13:01 -0400

    Error ESP32

--- src/esp32/esp32.routes.js
+++ src/esp32/esp32.routes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const esp32Controller = require('./esp32.controller');
 
-router.get('/', esp32Controller.get);
-router.get('/:deviceId', esp32Controller.getAll);
+//router.get('/', esp32Controller.get);
+//router.get('/:deviceId', esp32Controller.getAll);
 router.post('/', esp32Controller.post);
-router.delete('/:id', esp32Controller.delete); // Ruta para eliminar un dispositivo ESP32
+//router.delete('/:id', esp32Controller.delete); // Ruta para eliminar un dispositivo ESP32
 
 module.exports = router;
