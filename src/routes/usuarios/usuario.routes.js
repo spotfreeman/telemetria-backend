4ba60c3296ebbe652226ef31a3f0d468e282a9ad
spotfreeman <r.oyarzun83@gmail.com>
2025-09-10 23:40:03 -0300

    Cambiando sistema de usuarios

--- src/routes/usuarios/usuario.routes.js
+++ src/routes/usuarios/usuario.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const registerController = require('../controllers/usuarios/register.controller');
-const loginController = require('../controllers/usuarios/login.controller');
+const registerController = require('../../controllers/usuarios/register.controller');
+const loginController = require('../../controllers/usuarios/login.controller');
 
 router.post('/register', registerController.register);
 router.post('/login', loginController.login);
